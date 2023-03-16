fd12337594b560a0343333b0c2ace7491d4efe32
neocool <neokailthas@gmail.com>
2023-03-16 01:23:22 -0400

    Bot finished and in working condition

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def clickTextOnScreen(text,screenWidth, screenHeight):
                 pyautogui.moveTo(x + region[0], y + region[1]) # Add the region offset to get absolute coordinates on screen
                 pyautogui.click()
 
-def clickImageOnScreen(imageFile, screenWidth, screenHeight):
+def clickImageOnScreen(imageFile, screenWidth, screenHeight,confidence=0.9):
     # Define a region of interest on the screen (top, left, width, height)
     region = (0, 0, screenWidth, screenHeight)
 
@@ -41,7 +41,7 @@ def clickImageOnScreen(imageFile, screenWidth, screenHeight):
     screenshot = ImageGrab.grab(bbox=region)
 
     # Find the location of the image on the screenshot using pyautogui
-    location = pyautogui.locate(imageFile, screenshot,confidence=0.9)
+    location = pyautogui.locate(imageFile, screenshot,confidence=confidence)
 
     # If the image is found on the screen
     if location is not None:
@@ -125,83 +125,129 @@ def merc_tp_barrens_hq(screenWidth, screenHeight):
     clickImageOnScreen("images/TP_Choose.png",screenWidth, screenHeight)
     time.sleep(4)
 
-    #Select the mission
-    clickImageOnScreen("images/fq.png",screenWidth, screenHeight)
-    time.sleep(1)
-
-    #Select the choose button    
-    clickImageOnScreen("images/Choose.png",screenWidth, screenHeight)
-    time.sleep(4)
-
-    #Select the bot hero group    
-    clickImageOnScreen("images/bot_hero_group.png",screenWidth, screenHeight)
-    time.sleep(1)
-
-    #Select the choose button    
-    clickImageOnScreen("images/Choose.png",screenWidth, screenHeight)
-    time.sleep(1)
-
-    #Select the Lock in button    
-    clickImageOnScreen("images/LockIn.png",screenWidth, screenHeight)
-    time.sleep(5)
-
-    #Select the Play in button
-    time.sleep(1)
-    clickImageOnScreen("images/Play.png",screenWidth, screenHeight)
-    time.sleep(20)
-
     while True:
-        #Select Heros    
-        endTurn()
-        time.sleep(2)
-
-        #Play rounds
-        time.sleep(2)
-        print("Playing rounds")
-        while is_image_on_screen("images/PickTreas.png",screenWidth, screenHeight) == False:
-            time.sleep(1)
-            while is_image_on_screen("images/FightYellow.png",screenWidth, screenHeight) :
-                time.sleep(1)
-                #Select first Ability
-                pyautogui.moveTo(screenWidth * 0.3984, screenHeight *  0.4444)
-                pyautogui.click()
-                time.sleep(2)
-                gab = 0
-                while (not is_image_on_screen("images/AbillitiesBoarderWarrior.png",screenWidth, screenHeight)) and ( not is_image_on_screen("images/AbillitiesBoarderFighter.png",screenWidth, screenHeight) and ( not is_image_on_screen("images/AbillitiesBoarderCaster.png",screenWidth, screenHeight)) and ( not is_image_on_screen("images/AbillitiesBoarderNetrual.png",screenWidth, screenHeight)) and is_image_on_screen("images/FightYellow.png",screenWidth, screenHeight)) :
-                    #trying to hit enemy
-                    pyautogui.moveTo((screenWidth * 0.2507) + gab, screenHeight * 0.2715)
-                    pyautogui.click()
-                    time.sleep(0.5)
-                    gab += 100
-            
-            endTurn()
-            time.sleep(2)
-        
-        #Click take to take treasure
+        #Select the mission
+        print("Selecting Mission")
+        clickImageOnScreen("images/fq.png",screenWidth, screenHeight)
         time.sleep(1)
-        print("choosing treasure")
-        pyautogui.moveTo((screenWidth * 0.5) + gab, screenHeight * 0.5)
-        pyautogui.click()
-        time.sleep(3)
-        print("clicking take")
-        clickImageOnScreen("images/TakeTreasure.png",screenWidth, screenHeight)
-        time.sleep(2)
 
-        #Chose next battle
-        gab=0
-        while (not is_image_on_screen("images/Play.png",screenWidth, screenHeight)):
-            pyautogui.moveTo((screenWidth * 0.2) + gab, screenHeight * 0.5)
-            pyautogui.click()
-            time.sleep(0.5)
-            gab +=100
-        
+        #Select the choose button
+        print("Clicking choose")  
+        clickImageOnScreen("images/Choose.png",screenWidth, screenHeight)
+        time.sleep(4)
+
+        #Select the bot hero group    
+        print("selecting hero group")
+        clickImageOnScreen("images/bot_hero_group.png",screenWidth, screenHeight)
+        time.sleep(1)
+
+        #Select the choose button    
+        print("selecting choose")
+        clickImageOnScreen("images/Choose.png",screenWidth, screenHeight)
+        time.sleep(1)
+
+        #Select the Lock in button    
+        print("selecing lock in")
+        clickImageOnScreen("images/LockIn.png",screenWidth, screenHeight)
+        time.sleep(5)
+
         #Select the Play in button
+        print("selecting play button")
         time.sleep(1)
         clickImageOnScreen("images/Play.png",screenWidth, screenHeight)
-        time.sleep(20)
+        
+        last_battle = 0
+        while True:
+            print("sleeping for 20 seconds")
+            time.sleep(15)
+            #Select Heros 
+            print("ending turn to select heros")   
+            endTurn()           
 
-    #endTurn()
-    
+            #Play rounds
+            time.sleep(5)
+            print("Playing rounds")            
+            while (is_image_on_screen("images/PickTreas.png",screenWidth, screenHeight) == False):
+                if is_image_on_screen("images/Barrens_1st_Boss_victory.png",screenWidth, screenHeight):
+                    print("boss victory image on screen, breaking")
+                    break
+                time.sleep(1)
+                while is_image_on_screen("images/FightYellow.png",screenWidth, screenHeight) :
+                    time.sleep(1)
+                    #Select first Ability
+                    print("selecting first ability")
+                    pyautogui.moveTo(screenWidth * 0.3984, screenHeight *  0.4444)
+                    pyautogui.click()
+                    time.sleep(4)
+                    gab = 0
+                    while (not is_image_on_screen("images/AbillitiesBoarderWarrior.png",screenWidth, screenHeight)) and ( not is_image_on_screen("images/AbillitiesBoarderFighter.png",screenWidth, screenHeight) and ( not is_image_on_screen("images/AbillitiesBoarderCaster.png",screenWidth, screenHeight)) and ( not is_image_on_screen("images/AbillitiesBoarderNetrual.png",screenWidth, screenHeight)) and is_image_on_screen("images/FightYellow.png",screenWidth, screenHeight)) :
+                        #trying to hit enemy
+                        print("trying to hit enemy")
+                        pyautogui.moveTo((screenWidth * 0.2507) + gab, screenHeight * 0.2715)
+                        pyautogui.click()
+                        time.sleep(0.5)
+                        gab += 100
+                
+                endTurn()
+            
+            if last_battle == 1:
+                break
+
+            #Click take to take treasure
+            while (is_image_on_screen("images/PickTreas.png",screenWidth, screenHeight) == False):
+                time.sleep(5)
+
+            print("choosing treasure")
+            pyautogui.moveTo((screenWidth * 0.401171875), screenHeight * 0.4736111111111111)
+            pyautogui.click()
+            time.sleep(3)
+
+            print("clicking take")
+            clickImageOnScreen("images/TakeTreasure.png",screenWidth, screenHeight,0.8)
+            time.sleep(4)
+
+            #Chose next battle
+            gab=0
+            while (not is_image_on_screen("images/Play.png",screenWidth, screenHeight)):
+                pyautogui.moveTo((screenWidth * 0.2) + gab, screenHeight * 0.5)
+                pyautogui.click()
+                time.sleep(0.5)
+                gab +=100
+            
+            if is_image_on_screen("images/Barrens_1st_Boss.png",screenWidth, screenHeight):
+                last_battle = 1
+
+            #Select the Play in button
+            time.sleep(1)
+            clickImageOnScreen("images/Play.png",screenWidth, screenHeight)
+            time.sleep(20)
+
+        #collect rewards
+        time.sleep(10)
+        pyautogui.click()
+        time.sleep(20)
+        pyautogui.moveTo(screenWidth * 0.509765625, screenHeight *  0.2361111111111111)
+        pyautogui.click()
+        pyautogui.moveTo(screenWidth * 0.3546875, screenHeight *  0.7)
+        pyautogui.click()
+        pyautogui.moveTo(screenWidth * 0.69140625, screenHeight *  0.7180555555555556)
+        pyautogui.click()
+        
+        #Click Done button
+        time.sleep(1)
+        clickImageOnScreen("images/DoneButton.png",screenWidth, screenHeight)
+        #Complete bounty
+        time.sleep(2)
+        pyautogui.moveTo(screenWidth * 0.2, screenHeight *  0.2)
+        time.sleep(1)
+        clickImageOnScreen("images/OKButton.png",screenWidth, screenHeight)
+        time.sleep(1)
+        pyautogui.moveTo(screenWidth * 0.05, screenHeight *  0.05)
+        time.sleep(2)
+        pyautogui.click()
+        time.sleep(2)
+        last_battle = 0
+
 def getMousePercentage(screenWidth, screenHeight):
     # Get the current mouse coordinates
     mouseX, mouseY = pyautogui.position()
@@ -213,6 +259,14 @@ def getMousePercentage(screenWidth, screenHeight):
     # Return a tuple of (xPercent, yPercent)
     return (xPercent, yPercent)
 
+def capture_image(screenWidth, screenHeight,imageName,xsize,ysize):
+    # Get mouse location in percentage
+    xPercent, yPercent = getMousePercentage(screenWidth, screenHeight)
+    print(xPercent, yPercent)
+    top, left, width, height = (xPercent * screenWidth ) , (yPercent * screenHeight)  ,(xPercent * screenWidth)  + xsize, (yPercent * screenHeight) + ysize
+    save_screenshot(top, left, width, height ,"images/"+imageName+".png")
+
+    #clickImageOnScreen("images/"+imageName+".png",screenWidth, screenHeight)
 
 # Define the main function
 def main():
@@ -225,13 +279,8 @@ def main():
     merc_tp_barrens_hq(screenWidth, screenHeight)
     input()
 
-    # Get mouse location in percentage
-    xPercent, yPercent = getMousePercentage(screenWidth, screenHeight)
-    print(xPercent, yPercent)
-    top, left, width, height = (xPercent * screenWidth ) , (yPercent * screenHeight)  ,(xPercent * screenWidth)  + 100, (yPercent * screenHeight) + 50
-    #save_screenshot(top, left, width, height ,"images/TakeTreasure.png")
-
-    clickImageOnScreen("images/TakeTreasure.png",screenWidth, screenHeight)
+    #capture_image(screenWidth, screenHeight,"Barrens_1st_Boss_victory",200,300)
+    
 
 
 
